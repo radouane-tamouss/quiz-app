17fd7011b55bf400ba8f40207fd86bc06a7dcaa0
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-02-12 09:42:43 +0100

    add files

---
+++ js/quest.js
@@ -0,0 +1,72 @@
+ // création d'un tableau et transmission du nombre, des questions, des options et des réponses
+let questions = [
+    {
+    numb: 1,
+    question: "  En gestion de projet qui appelle-t-on maîtrise d'ouvrage?",
+    answer: "la société chargée de l'accompagnement",
+    options: [
+      " le client",
+      "le prestataire",
+      "la société chargée de l'accompagnement",
+      "les utilisateurs"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quel code de réponse HTTP 1.0 est faux?",
+    answer: "500 : requête acceptée, traitement en cours ",
+    options: [
+      "200 : la requête s'est déroulée correctement",
+      "400 : erreur de syntaxe dans la requête envoyé par le client",
+      "500 : requête acceptée, traitement en cours ",
+      "404 : la ressource demandée n'existe pas"
+    ]
+  },
+    {
+    numb: 3,
+    question: "UML est ?",
+    answer: "un langage de modélisation",
+    options: [
+      "un langage de modélisation",
+      "un type de port",
+      " La partie « données » de la méthode MERISE",
+      "un standard de communication"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Sous Windows XP, la configuration est enregistré dans  ?",
+    answer: "La base de registre",
+    options: [
+      "La base de composants de windows",
+      "Le fichier win.ini",
+      "La base de registre",
+      "Le fichier autoexec.bat"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Une URL (Uniform Resource Locator) est composé obligatoirement de certains éléments, ci dessous, un est facultatif :",
+    answer: " du port",
+    options: [
+      "du port",
+      "du nom du serveur",
+      "cdu protocole de communication",
+      "tous les response faux"
+    ]
+  },
+  // vous pouvez décommenter les codes ci-dessous et les dupliquer autant que vous voulez ajouter une question
+   // mais rappelez-vous que vous devez donner la valeur numérique sérialiser comme 1,2,3,5,6,7,8,9 .....
+
+   // {
+   // engourdi : 6,
+   // question : "Votre question est ici",
+   // réponse : "La bonne réponse à la question est ici",
+   // choix : [
+   //     "Option 1",
+   //     "Option 2",
+   // "possibilité 3",
+   // "possibilité 4"
+   // ]
+   // },
+];
